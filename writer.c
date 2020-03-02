094bee028048feb3ab452bc6ed90bc3dac667740
Saac Sornchai <sornchai.la@ku.th>
2020-03-02 14:15:02 +0700

    add example of sprintf

--- writer.c
+++ writer.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
     FILE* fp;
     char data2write[100] = "I study in Computer Science, Kasetsart University";
+    char str[100];
 
     fp = fopen("output.out", "w");
 
@@ -12,6 +12,8 @@ int main() {
         fputs(data2write, fp);
         fputs("\n", fp);
         fprintf(fp, "%d - %d = %d\n", 3000, 1234, 3000 - 1234);
+        sprintf(str, "%d%08d", 5, 1);
+        fprintf(fp, "%s\n", str);
         puts("Writing file has been finished");
         fclose(fp);
     } else {
